f3a97b72e152cfec215b9a7acaa309e56c8842ec
artemry-nv <artemry@nvidia.com>
2021-03-10 01:21:14 +0300

    Debug
    Signed-off-by: artemry-nv <artemry@nvidia.com>

--- .ci/scripts/run_fb_dlrm_docker.sh
+++ .ci/scripts/run_fb_dlrm_docker.sh
@@ -27,21 +27,34 @@ DOCKER_RUN_ARGS="\
 "
 DOCKER_SSH_PORT="12345"
 DOCKER_CONTAINER_NAME="torch_ucc_ci"
-DOCKER_IMAGE_NAME=""
+DOCKER_IMAGE_NAME="${TORCH_UCC_DOCKER_IMAGE_NAME}:${BUILD_ID}"
 
 while read -r HOST; do
     echo "INFO: HOST = $HOST"
-    STALE_DOCKER_CONTAINER_LIST=$(sudo ssh $CUR_HOST "docker ps -a -q -f name=${DOCKER_CONTAINER_NAME}")
-    if [ -n "${STALE_DOCKER_CONTAINER_LIST}" ]
-    then
+    STALE_DOCKER_CONTAINER_LIST=$(sudo ssh "$HOST" "docker ps -a -q -f name=${DOCKER_CONTAINER_NAME}")
+    if [ -n "${STALE_DOCKER_CONTAINER_LIST}" ]; then
         echo "WARNING: stale docker container (name: ${DOCKER_CONTAINER_NAME}) is detected on ${HOST} (to be stopped)"
         echo "INFO: Stopping stale docker container (name: ${DOCKER_CONTAINER_NAME}) on ${HOST}..."
         sudo ssh "${HOST}" docker stop ${DOCKER_CONTAINER_NAME}
         echo "INFO: Stopping stale docker container (name: ${DOCKER_CONTAINER_NAME}) on ${HOST}... DONE"
     fi
 
+    echo "INFO: start docker container on $HOST ..."
     sudo ssh "$HOST" "docker run \
         ${DOCKER_RUN_ARGS} \
         ${DOCKER_IMAGE_NAME} \
         bash -c "/usr/sbin/sshd -p ${DOCKER_SSH_PORT}; sleep infinity""
-done < "$HOSTFILE"
+    echo "INFO: start docker container on $HOST ... DONE"
+
+    echo "INFO: verify docker container on $HOST ..."
+    sudo ssh "$HOST" -p ${DOCKER_SSH_PORT} hostname
+    echo "INFO: verify docker container on $HOST ... DONE"
+done <"$HOSTFILE"
+
+sleep 20000
+
+while read -r HOST; do
+    echo "INFO: stop docker container on $HOST ..."
+    sudo ssh "${HOST}" docker stop ${DOCKER_CONTAINER_NAME}
+    echo "INFO: stop docker container on $HOST ... DONE"
+done <"$HOSTFILE"
