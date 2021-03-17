c8a07a5e0bf3314031be3d50c5fc40c839d21104
artemry-nv <artemry@nvidia.com>
2021-03-18 02:40:00 +0300

    Debug
    Signed-off-by: artemry-nv <artemry@nvidia.com>

--- .ci/scripts/install_torch_ucc.sh
+++ .ci/scripts/install_torch_ucc.sh
@@ -1,14 +1,31 @@
 #!/bin/bash -eEx
 set -o pipefail
 
-echo "INFO: Install Torch-UCC"
-cd "${TORCH_UCC_SRC_DIR}"
+echo "INFO: Install Torch-UCC (UCC version)"
+cd "${TORCH_UCC_PYTHON_VENV_DIR}"
+
+# UCC
+python3 -m venv --system-site-packages ucc
+. "${TORCH_UCC_PYTHON_VENV_DIR}/ucc/bin/activate"
 export UCX_HOME=${UCX_INSTALL_DIR}
-export XCCL_HOME=${XCCL_INSTALL_DIR}
-# TODO debug
+export UCC_HOME=${UCC_INSTALL_DIR}
 export WITH_CUDA=${CUDA_HOME}
-#export WITH_CUDA=no
+cd "${TORCH_UCC_SRC_DIR}"
 python setup.py install bdist_wheel
 pip3 list | grep torch
 python -c 'import torch, torch_ucc'
 cp "${TORCH_UCC_SRC_DIR}/dist/"*.whl "${TORCH_UCC_PKG_DIR}"
+deactivate
+
+# XCCL
+python3 -m venv --system-site-packages xccl
+. "${TORCH_UCC_PYTHON_VENV_DIR}/xccl/bin/activate"
+export UCX_HOME=${UCX_INSTALL_DIR}
+export XCCL_HOME=${XCCL_INSTALL_DIR}
+export WITH_CUDA=${CUDA_HOME}
+cd "${TORCH_UCC_SRC_DIR}"
+python setup.py install bdist_wheel
+pip3 list | grep torch
+python -c 'import torch, torch_ucc'
+cp "${TORCH_UCC_SRC_DIR}/dist/"*.whl "${TORCH_UCC_PKG_DIR}"
+deactivate
