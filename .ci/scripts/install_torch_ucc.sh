3f04498c5e854fb7e37ae428a756d3c7bee58633
artemry-nv <artemry@nvidia.com>
2021-03-18 10:41:48 +0300

    Debug
    Signed-off-by: artemry-nv <artemry@nvidia.com>

--- .ci/scripts/install_torch_ucc.sh
+++ .ci/scripts/install_torch_ucc.sh
@@ -14,25 +14,5 @@ git clean -ffdx
 python setup.py install bdist_wheel
 pip3 list | grep torch
 python -c 'import torch, torch_ucc'
-TORCH_UCC_WHL_PKG_FULL_PATH=$(ls -1 "${TORCH_UCC_SRC_DIR}/dist/"*.whl)
-TORCH_UCC_WHL_PKG_BASENAME=$(basename "${TORCH_UCC_WHL_PKG_FULL_PATH}" .whl)
-mv "${TORCH_UCC_WHL_PKG_FULL_PATH}" "${TORCH_UCC_PKG_DIR}/${TORCH_UCC_WHL_PKG_BASENAME}_ucc.whl"
-deactivate
-
-# XCCL
-echo "INFO: Install Torch-UCC (XCCL version)"
-cd "${TORCH_UCC_PYTHON_VENV_DIR}"
-python3 -m venv --system-site-packages xccl
-. "${TORCH_UCC_PYTHON_VENV_DIR}/xccl/bin/activate"
-export UCX_HOME=${UCX_INSTALL_DIR}
-export XCCL_HOME=${XCCL_INSTALL_DIR}
-export WITH_CUDA=${CUDA_HOME}
-cd "${TORCH_UCC_SRC_DIR}"
-git clean -ffdx
-python setup.py install bdist_wheel
-pip3 list | grep torch
-python -c 'import torch, torch_ucc'
-TORCH_UCC_WHL_PKG_FULL_PATH=$(ls -1 "${TORCH_UCC_SRC_DIR}/dist/"*.whl)
-TORCH_UCC_WHL_PKG_BASENAME=$(basename "${TORCH_UCC_WHL_PKG_FULL_PATH}" .whl)
-mv "${TORCH_UCC_WHL_PKG_FULL_PATH}" "${TORCH_UCC_PKG_DIR}/${TORCH_UCC_WHL_PKG_BASENAME}_xccl.whl"
+cp "${TORCH_UCC_SRC_DIR}/dist/"*.whl "${TORCH_UCC_PKG_DIR}"
 deactivate
--- .ci/scripts/run_tests_ucc.sh
+++ .ci/scripts/run_tests_ucc.sh
@@ -3,4 +3,6 @@ set -o pipefail
 
 UCC_SRC_DIR="${TORCH_UCC_SRC_DIR}/ucc"
 cd "${UCC_SRC_DIR}/build"
+
+export UCX_WARN_UNUSED_ENV_VARS=n
 make gtest
