13053b72bb40ca1ffb08d6728d5fc1cc9a09d96d
artemry-nv <artemry@nvidia.com>
2021-03-22 09:26:58 +0300

    Debug
    Signed-off-by: artemry-nv <artemry@nvidia.com>

--- .ci/scripts/configure_python.sh
+++ .ci/scripts/configure_python.sh
@@ -1,5 +1,26 @@
 #!/bin/bash -eEx
 set -o pipefail
 
-alternatives --set python /usr/bin/python3
+# Install conda
+cd /tmp
+curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -p /opt/conda -b
+rm -f Miniconda3-latest-Linux-x86_64.sh
+export PATH /opt/conda/bin:${PATH}
+
+# Install conda python
+conda update -y conda
+conda install -c anaconda -y \
+    python \
+    pip \
+    scikit-learn
+pip3 install --no-cache-dir python-hostlist
+
+alternatives --set python /opt/conda/bin/python3
+#alternatives --set python /usr/bin/python3
 pip3 install --user --upgrade setuptools wheel
+
+python --version
+python3 --version
+command -v python
+command -v python3
